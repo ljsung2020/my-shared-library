a7d767e27bb0bd11168ed76bb0c1269ce82c6d81
infobee <ljsung2020>
2025-07-30 11:02:21 +0900

    Add config.yaml support for deployApp

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,4 +1,15 @@
+@Grab('org.yaml:snakeyaml:1.30')
+import org.yaml.snakeyaml.Yaml
+
 def call(String env) {
-	def deployer = new com.mycompany.Deployment(this)
-	deployer.run(env)
+	def configText = libraryResource('config.yaml')
+	def yaml = new Yaml()
+	def config = yaml.load(configText)
+	
+	def envConfig = config[env]
+	echo "Deploying ${env} with image ${envConfig.image}"
+	sh "docker rm -f ${env}-container || true"
+	sh "docker run -d --name ${env}-container -e ENV=${env} ${envConfig.image}"
+//	def deployer = new com.mycompany.Deployment(this)
+//	deployer.run(env)
 }
