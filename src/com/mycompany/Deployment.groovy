c3f058dbea89663f1255957ee617a69c4dc2ed1a
infobee <ljsung2020>
2025-07-30 10:50:12 +0900

    Fix: remove existing container vefore deploy

--- src/com/mycompany/Deployment.groovy
+++ src/com/mycompany/Deployment.groovy
@@ -9,6 +9,7 @@ class Deployment {
 	
 	def run(String env) {
 	    steps.echo "Running Deployment for ${env}"
+	    steps.sh "docker rm -f ${env}-container || true"
 	    steps.sh "docker run -d --name ${env}-container -e ENV=${env} nginx:alpine"
 	}
 }
